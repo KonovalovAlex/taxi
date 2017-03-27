dab77187b3dd546a14ec4eaae69ac9e4e697896e
KonovalovAlex <yalexnicetomeetu@gmal.com>
2017-03-27 12:59:19 +0600

    Initial commit

---
+++ src/main/java/ConnectionFactory.java
@@ -0,0 +1,30 @@
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+/**
+ * Created by Alex on 20.03.2017.
+ */
+public class ConnectionFactory {
+
+    DataSource dataSource = null;
+
+    public void init() throws SQLException {
+        try {
+            Context envCtx = (Context) (new InitialContext().lookup("java:comp/env"));
+            DataSource ds = (DataSource) envCtx.lookup("jdbc/testphones");
+            Connection cn = ds.getConnection();
+            cn.close();
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World77!</h2>
+</body>
+</html>
